d0e4bb3dd5f0164228e86f7da1ca1e8807f4a116
Fransiskus <aldzaidea@gmail.com>
2022-03-10 18:51:52 +0000

    add dashboard count layout

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"]
+  
   def show
+    @total_product = Product.count
+    @total_categories = Category.count
+ 
   end
 end
